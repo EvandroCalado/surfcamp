038aa0e472c472d206c8c03cc78027da183b6751
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-11-05 00:37:34 -0300

    :feat: fetch real data (home in progress)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,43 +1,18 @@
+import { InfoClass } from "@/api/info";
 import { Info, SectionHero } from "@/components/Shared";
 
-export default function Home() {
-  const strapiHeadline = (
-    <>
-      <h1>entube.</h1>
-      <h1>sua.</h1>
-      <h1>felicidade.</h1>
-    </>
-  );
+const infoCtrl = new InfoClass();
 
-  const strapiInfo = {
-    headline: "a experiência.",
-    text: (
-      <p className="copy">
-        At Sam’s Surfcamp, we invite you to embark on an unforgettable surfing
-        adventure. Nestled in the heart of [Location] our surf camp offers an
-        exhilarating experience for beginners, intermediate surfers, and
-        seasoned wave riders alike. Dive into the world of surfing with our
-        expert instructors who have years of experience and a deep passion for
-        the sport. Whether you’re a first-time surfer looking to catch your
-        first wave or a seasoned pro seeking to enhance your skills, our
-        dedicated team is here to guide you every step of the way. Immerse
-        yourself in the natural beauty of our surf camp’s surroundings. Picture
-        yourself waking up to the sound of crashing waves and feeling the warm
-        sand beneath your feet. With pristine beaches and a vibrant coastal
-        atmosphere, [Location] sets the perfect stage for your surf adventure.
-      </p>
-    ),
-    button: (
-      <button className="btn btn--medium btn--turquoise">Reservar</button>
-    ),
-    reversed: false,
-  };
+export default async function Home() {
+  const data = await infoCtrl.get();
+  const { section_hero, infos } = data.attributes;
 
   return (
     <main>
-      <SectionHero image="/assets/hero-home.jpeg" headline={strapiHeadline} />
-      <Info info={strapiInfo} />
-      <Info info={{ ...strapiInfo, reversed: true }} />
+      <SectionHero section_hero={section_hero} />
+      {infos.data.map((info) => (
+        <Info key={info.id} info={info} />
+      ))}
     </main>
   );
 }
