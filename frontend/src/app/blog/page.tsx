28c361f416ad47c63dd3d0e79f39b201d9fed7a5
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-11-06 18:04:41 -0300

    :feat: add blog page

---
+++ frontend/src/app/blog/page.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { BlogClass } from "@/api/blog";
+import { HighlightArticle, SubscribeToNewsletter } from "@/components/Blog";
+import { FeaturedItems } from "@/components/Featured";
+import { Loading } from "@/components/Shared";
+import { TypeBlog } from "@/types/typeBlog";
+import { useEffect, useState } from "react";
+
+const blogCtrl = new BlogClass();
+
+export default function Blog() {
+  const [highlightArticle, setHighlightArticle] = useState<TypeBlog>();
+  const [featuredItemsArticles, setFeaturedItemsArticles] = useState<
+    TypeBlog[]
+  >([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageCount, setPageCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      setDisabled(true);
+
+      try {
+        const highlight = await blogCtrl.getHighlightArticle();
+        setHighlightArticle(highlight);
+
+        const articles = await blogCtrl.getfeaturedItemsArticles(currentPage);
+        setFeaturedItemsArticles([
+          ...featuredItemsArticles,
+          ...articles?.data!,
+        ]);
+        setPageCount(articles!.meta.pagination.pageCount);
+
+        setLoading(false);
+        setDisabled(false);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPage]);
+
+  const handleOnClick = () => {
+    setCurrentPage(currentPage + 1);
+  };
+
+  if (!featuredItemsArticles) return null;
+  if (!highlightArticle) return null;
+
+  return (
+    <main className="blog">
+      <HighlightArticle HighlightArticle={highlightArticle!} />
+      <SubscribeToNewsletter />
+      <FeaturedItems
+        featuredItems={featuredItemsArticles!}
+        headline="Nossos Artigos em Destaque"
+      />
+
+      <div className="blog__button-container">
+        <button
+          disabled={pageCount === currentPage ? true : disabled}
+          onClick={handleOnClick}
+          className="btn btn--medium btn--turquoise blog__button"
+        >
+          {loading ? <Loading /> : "Veja mais"}
+        </button>
+      </div>
+    </main>
+  );
+}
