8fd21cc70904c418af1c4c59a666fb3c765ecfa0
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-11-06 19:40:31 -0300

    :feat: add featured card

---
+++ frontend/src/components/Featured/FeaturedCard.tsx
@@ -0,0 +1,30 @@
+import { TypeBlog } from "@/types/typeBlog";
+import { formatDate } from "@/utils/format-date";
+import Image from "next/image";
+import Link from "next/link";
+
+export type FeaturedCardProps = {
+  article: TypeBlog;
+};
+
+export function FeaturedCard({ article }: FeaturedCardProps) {
+  const { slug, image, headline, createdAt } = article.attributes;
+
+  return (
+    <Link href={`/blog/${slug}`} className="featured-card">
+      <div className="featured-card__image">
+        <Image
+          src={image.data.attributes.formats.small!.url}
+          alt={headline}
+          fill
+        />
+      </div>
+
+      <div className="featured-card__content">
+        <h5 className="featured-card__content--headline">{headline}</h5>
+
+        <p className="copy">{formatDate(createdAt)}</p>
+      </div>
+    </Link>
+  );
+}
