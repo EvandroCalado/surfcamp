20481acd1fc06b43fc75411ef1a85c7c10dbad33
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-11-06 23:48:05 -0300

    :type: add types for blog

---
+++ frontend/src/types/typeBlog.ts
@@ -0,0 +1,359 @@
+export interface TypeBlogs {
+  data: TypeBlog[];
+  meta: Meta;
+}
+
+interface Meta {
+  pagination: TypePagination;
+}
+
+export interface TypePagination {
+  page: number;
+  pageSize: number;
+  pageCount: number;
+  total: number;
+}
+
+export interface TypeBlog {
+  id: number;
+  attributes: Attributes5;
+}
+
+interface Attributes5 {
+  headline: string;
+  excerpt: string;
+  slug: string;
+  isHighlightArticle: boolean;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  image: Image;
+  author: Author;
+  articleContent: ArticleContent[];
+}
+
+interface ArticleContent {
+  id: number;
+  __component: string;
+  content?: string;
+  imageCaption?: string;
+  isLandscape?: boolean;
+  imageShowsRight?: boolean;
+  image?: Image3;
+  indexName: string;
+}
+
+interface Image3 {
+  data: Data4;
+}
+
+interface Data4 {
+  id: number;
+  attributes: Attributes4;
+}
+
+interface Attributes4 {
+  name: string;
+  alternativeText?: any;
+  caption?: any;
+  width: number;
+  height: number;
+  formats: Formats3;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl?: any;
+  provider: string;
+  provider_metadata: Providermetadata;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface Formats3 {
+  thumbnail: Thumbnail;
+  small: Thumbnail;
+  medium?: Thumbnail;
+}
+
+interface Author {
+  data: Data3;
+}
+
+interface Data3 {
+  id: number;
+  attributes: Attributes3;
+}
+
+interface Attributes3 {
+  name: string;
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  image: Image2;
+}
+
+interface Image2 {
+  data: Data2;
+}
+
+interface Data2 {
+  id: number;
+  attributes: Attributes2;
+}
+
+interface Attributes2 {
+  name: string;
+  alternativeText?: any;
+  caption?: any;
+  width: number;
+  height: number;
+  formats: Formats2;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl?: any;
+  provider: string;
+  provider_metadata: Providermetadata;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface Formats2 {
+  thumbnail: Thumbnail;
+}
+
+interface Image {
+  data: Data;
+}
+
+interface Data {
+  id: number;
+  attributes: Attributes;
+}
+
+interface Attributes {
+  name: string;
+  alternativeText?: any;
+  caption?: any;
+  width: number;
+  height: number;
+  formats: Formats;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl?: any;
+  provider: string;
+  provider_metadata: Providermetadata;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface Formats {
+  thumbnail: Thumbnail;
+  small?: Thumbnail;
+  medium?: Thumbnail;
+}
+
+interface Thumbnail {
+  name: string;
+  hash: string;
+  ext: string;
+  mime: string;
+  path?: any;
+  width: number;
+  height: number;
+  size: number;
+  url: string;
+  provider_metadata: Providermetadata;
+}
+
+interface Providermetadata {
+  public_id: string;
+  resource_type: string;
+}
+interface RootObject {
+  data: Datum[];
+  meta: Meta;
+}
+
+interface Meta {
+  pagination: Pagination;
+}
+
+interface Pagination {
+  page: number;
+  pageSize: number;
+  pageCount: number;
+  total: number;
+}
+
+interface Datum {
+  id: number;
+  attributes: Attributes5;
+}
+
+interface Attributes5 {
+  headline: string;
+  excerpt: string;
+  slug: string;
+  isHighlightArticle: boolean;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  image: Image;
+  author: Author;
+  articleContent: ArticleContent[];
+}
+
+interface ArticleContent {
+  id: number;
+  __component: string;
+  content?: string;
+  imageCaption?: string;
+  isLandscape?: boolean;
+  imageShowsRight?: boolean;
+  image?: Image3;
+}
+
+interface Image3 {
+  data: Data4;
+}
+
+interface Data4 {
+  id: number;
+  attributes: Attributes4;
+}
+
+interface Attributes4 {
+  name: string;
+  alternativeText?: any;
+  caption?: any;
+  width: number;
+  height: number;
+  formats: Formats3;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl?: any;
+  provider: string;
+  provider_metadata: Providermetadata;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface Formats3 {
+  thumbnail: Thumbnail;
+  small: Thumbnail;
+  medium?: Thumbnail;
+}
+
+interface Author {
+  data: Data3;
+}
+
+interface Data3 {
+  id: number;
+  attributes: Attributes3;
+}
+
+interface Attributes3 {
+  name: string;
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  image: Image2;
+}
+
+interface Image2 {
+  data: Data2;
+}
+
+interface Data2 {
+  id: number;
+  attributes: Attributes2;
+}
+
+interface Attributes2 {
+  name: string;
+  alternativeText?: any;
+  caption?: any;
+  width: number;
+  height: number;
+  formats: Formats2;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl?: any;
+  provider: string;
+  provider_metadata: Providermetadata;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface Formats2 {
+  thumbnail: Thumbnail;
+}
+
+interface Image {
+  data: Data;
+}
+
+interface Data {
+  id: number;
+  attributes: Attributes;
+}
+
+interface Attributes {
+  name: string;
+  alternativeText?: any;
+  caption?: any;
+  width: number;
+  height: number;
+  formats: Formats;
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl?: any;
+  provider: string;
+  provider_metadata: Providermetadata;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface Formats {
+  thumbnail: Thumbnail;
+  small?: Thumbnail;
+  medium?: Thumbnail;
+}
+
+interface Thumbnail {
+  name: string;
+  hash: string;
+  ext: string;
+  mime: string;
+  path?: any;
+  width: number;
+  height: number;
+  size: number;
+  url: string;
+  provider_metadata: Providermetadata;
+}
+
+interface Providermetadata {
+  public_id: string;
+  resource_type: string;
+}
