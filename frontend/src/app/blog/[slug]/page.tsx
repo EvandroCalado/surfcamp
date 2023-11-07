1eb1db96c74368a3b4130186fcc4833ed9ce7e42
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-11-07 20:08:17 -0300

    :feat: add blog slug page

---
+++ frontend/src/app/blog/[slug]/page.tsx
@@ -0,0 +1,39 @@
+import { BlogClass } from "@/api/blog";
+import { ArticleContent, ArticleHeader } from "@/components/Blog";
+import { FeaturedItems } from "@/components/Featured";
+
+const blogCtrl = new BlogClass();
+
+export default async function BlogSlug({ params }: ParamsProps) {
+  const { slug } = params;
+  const article = await blogCtrl.getBySlug(slug);
+  const articles = await blogCtrl.getfeaturedItemsArticles();
+
+  if (!article) return null;
+  if (!articles) return null;
+
+  return (
+    <main>
+      <ArticleHeader article={article} />
+      <ArticleContent article={article} />
+      <FeaturedItems
+        headline="Artigos Relacionados"
+        featuredItems={articles.data}
+      />
+    </main>
+  );
+}
+
+export const generateStaticParams = async () => {
+  const blogs = await blogCtrl.getAll();
+
+  return blogs?.data.map((blog) => ({
+    slug: blog.attributes.slug,
+  }));
+};
+
+type ParamsProps = {
+  params: {
+    slug: string;
+  };
+};
