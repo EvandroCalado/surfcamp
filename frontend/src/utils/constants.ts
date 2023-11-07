3e864056f437cbab09fa2b7f3dfaa809b370746a
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-11-06 23:48:29 -0300

    :chore: contants for endpoints

---
+++ frontend/src/utils/constants.ts
@@ -0,0 +1,8 @@
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://127.0.0.1:1337/api";
+
+export const ENDPOINTS = {
+  INFO: `${BASE_URL}/info-page?populate=deep`,
+  EXPERIENCE: `${BASE_URL}/experience-page?populate=deep`,
+  BLOG: `${BASE_URL}/blog-articles?populate=deep`,
+};
