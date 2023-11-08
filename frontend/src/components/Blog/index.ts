0231538fc9aabf159fe98880090cd52897e4935e
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-11-08 20:50:14 -0300

    :chore: index for imports

--- frontend/src/components/Blog/index.ts
+++ frontend/src/components/Blog/index.ts
@@ -1,4 +1,6 @@
 export * from "./ArticleContent";
 export * from "./ArticleHeader";
+export * from "./BlogHome";
+export * from "./BlogHomeCard";
 export * from "./HighlightArticle";
 export * from "./SubscribeToNewsletter";
