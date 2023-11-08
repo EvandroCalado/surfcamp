5f1cf39bc40769a26e1415fb5ecccf4c0afac5a8
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-11-08 20:37:35 -0300

    :feat: add blog home component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,29 @@
+import { BlogClass } from "@/api/blog";
 import { InfoClass } from "@/api/info";
+import { BlogHome } from "@/components/Blog";
 import { Info, SectionHero } from "@/components/Shared";
 
 const infoCtrl = new InfoClass();
+const blogCtrl = new BlogClass();
 
 export default async function Home() {
   const data = await infoCtrl.get();
+  const articles = await blogCtrl.getArticlesHome();
+
+  if (!data) return null;
+  if (!articles) return null;
+
   const { section_hero, infos } = data.attributes;
 
   return (
     <main>
       <SectionHero section_hero={section_hero} />
+
       {infos.data.map((info) => (
         <Info key={info.id} info={info} />
       ))}
+
+      <BlogHome articles={articles} />
     </main>
   );
 }
